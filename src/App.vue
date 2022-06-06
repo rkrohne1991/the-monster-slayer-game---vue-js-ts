b3830c497ffb423ba326aff87454840f444ec7c1
Rafal <rafal.krohne@hotmail.co.uk>
2022-06-06 15:23:58 +0200

    setting up the data and base methods

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,23 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+const getRandomValue = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min)) + min;
+
 export default defineComponent({
   data() {
-    return {};
+    return { playerHealth: 100, monsterHealth: 100 };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
   },
 });
 </script>
